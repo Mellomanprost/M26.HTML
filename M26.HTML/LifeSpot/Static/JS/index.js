ea651b2599edb99532e0f20db4ae6dcab6120da6
Mellomanprost <anoppa@mail.ru>
2023-03-15 18:48:57 +0300

    completed tasks from unit 29.5

--- M26.HTML/LifeSpot/Static/JS/index.js
+++ M26.HTML/LifeSpot/Static/JS/index.js
@@ -1,36 +1,39 @@
-﻿function handleSession() {
-    // создадим объект Map для хранения сессии
-    let session = new Map();
-    // Сохраним UserAgent
-    session.set('userAgent', window.navigator.userAgent);
+﻿// функция для получения пользовательского ввода
+const inputParseFunction = function () {
+    return document.getElementsByTagName('input')[0].value.toLowerCase()
+};
 
+// объект Map для хранения сессии
+let session = new Map();
+
+function checkAge() {
     // Запросим возраст пользователя и сохраним в переменную
-    let userAge = prompt('Укажите свой возраст: ');
-    session.set('age', userAge);
+    session.set('age', prompt('Укажите пожалуйста свой возраст: '));
 
     // Проверка на возраст и сохранение сессии
     if (session.get('age') >= 18) {
-        let curDate = new Date().toLocaleString();
-
-        alert(`Приветствуем на LifeSpot!\nТекущее время: ${curDate}`); 7
-        session.set('currentDate', curDate);
+        alert(`Приветствуем на LifeSpot!\nТекущее время: ${new Date().toLocaleString()}`);
     } else {
         alert('Наш сайт только для лиц старше 18 лет! Вы будете перенаправлены');
         window.location.href = "http://www.google.com";
     }
-
-    return session;
 }
 
-let sessionLog = function logSession(session) {
+function handleSession() {
+    // Сохраним UserAgent
+    session.set('userAgent', window.navigator.userAgent);
+
+    // Сохраним время начала сессии
+    session.set('currentDate', new Date().toLocaleString());
+}
+/// Вывод данных сессии в консоль
+let sessionLog = function logSession() {
     for (let item of session) {
         console.log(item);
     }
-
 }
 
-
-function filterContent(userInput) {     // Принимаем пользовательский ввод в качестве параметра.
+function filterContent() {     // Принимаем пользовательский ввод в качестве параметра.
     // Находим контейнеры с видео, которые необходимо фильтровать
     let elements = document.getElementsByClassName('video-container');
 
@@ -40,7 +43,7 @@ function filterContent(userInput) {     // Принимаем пользоват
         let videoText = elements[i].querySelector('.video-title').innerText;
 
         // Выполняем фильтрацию, сравнивая значения в нижнем регистре
-        if (!videoText.toLowerCase().includes(userInput.toLowerCase())) {
+        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())) {
             // Скрываем неподходящие
             elements[i].style.display = 'none';
         } else {
